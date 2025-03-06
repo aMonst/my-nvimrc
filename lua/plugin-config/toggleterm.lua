632f941f14bdd8182f0d7899c048690d0320f4be
hao_liu <hao_liu@foxitsoftware.com>
2025-03-06 14:15:07 +0800

    集成yazi

--- lua/plugin-config/toggleterm.lua
+++ lua/plugin-config/toggleterm.lua
@@ -41,6 +41,32 @@ function lazygit_toggle()
     lazygit_term:toggle()
 end
 
+local yazi_term = Terminal:new({
+    cmd = 'yazi',
+    dir = vim.loop.cwd(),
+    hidden = true,
+    direction = "float",
+    on_exit = function(term) -- 退出时的回调
+        local tmpfile = vim.fn.expand("~/.yazi_selected")
+        if vim.fn.filereadable(tmpfile) == 1 then
+            local files = vim.fn.readfile(tmpfile)
+            for _, file in ipairs(files) do
+                vim.schedule(function()
+                    -- 打开选中文件（支持多文件）
+                    vim.cmd("edit " .. vim.fn.fnameescape(file))
+                end)
+            end
+            os.remove(tmpfile) -- 清理临时文件
+        end
+        term:close() -- 关闭终端窗口
+    end,
+});
+
+function yazi_toggle()
+    yazi_term:toggle()
+end
+
 vim.api.nvim_set_keymap("n", "<leader>py", "<Cmd>lua python_toggle()<CR>", {noremap = true, silent = true})
 vim.api.nvim_set_keymap("n", "<leader>ht", "<Cmd>lua htop_toggle()<CR>", {noremap = true, silent = true})
 vim.api.nvim_set_keymap("n", "<leader>lg", "<Cmd>lua lazygit_toggle()<CR>", {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "<leader>y", "<Cmd>lua yazi_toggle()<CR>", {noremap = true, silent = true})
